11e7f2a0cca7fbcd1e0cf108f8bdb46c194a3c91
mcmah309 <dillonmcmahon79@gmail.com>
2023-11-14 20:02:12 -0500

    feat: Add json and string as valid input

--- src/lib.rs
+++ src/lib.rs
@@ -3,46 +3,36 @@ extern crate proc_macro;
 use proc_macro2::TokenStream;
 use serde_json::Value;
 use syn::LitStr;
-use syn::parse::{Parse, ParseStream, Result};
+use syn::parse::{Parse, ParseStream};
 use tera::{Context, Tera};
 use unicode_segmentation::UnicodeSegmentation;
 
-struct TeraMacroInput {
-    context: LitStr,
-    rust_code: TokenStream,
-}
-
-impl Parse for TeraMacroInput {
-    fn parse(input: ParseStream) -> Result<Self> {
-        let json: LitStr = input.parse()?;
-        input.parse::<syn::token::Comma>()?;
-        let rust_code = input.parse()?;
-
-        Ok(TeraMacroInput { context: json, rust_code })
-    }
-}
-
 #[proc_macro]
 pub fn tera(tokens: proc_macro::TokenStream) -> proc_macro::TokenStream {
-    let input = syn::parse_macro_input!(tokens as TeraMacroInput);
-    let template = remove_space_added_by_parsing(&input.rust_code.to_string());
-    println!("template: {:?}", template);
+    let tokens_string = tokens.to_string();
+    if tokens_string.is_empty() {
+        return tokens;
+    }
+    let (context_string, template_string) = parse_into_context_and_code(&tokens_string, tokens);
+    println!("context: {}", context_string);
+    println!("template: {}", template_string);
+    let value: Value = serde_json::from_str(&context_string).expect("Could not parse context in valid json.");
+    println!("value: {}", value);
     let mut tera = Tera::default();
-    tera.add_raw_template("tera", &template).expect("The template was not valid.");
-    let json_string: String = input.context.value();
-    let value: Value = serde_json::from_str(&json_string).expect("The Context was not valid json.");
-    println!("value: {:?}", value);
+    let refined_template = remove_space_between_tera_brackets_added_by_parsing(&template_string);
+    println!("refined_template: {}", refined_template);
+    tera.add_raw_template("tera", &refined_template).expect("The template was not valid.");
     let context = Context::from_value(value).expect("Tera failed to create Context from json value.");
     let output: String = tera.render("tera", &context).expect("Could not render the template");
-    println!("output: {:?}", output);
-    let token_stream: TokenStream = syn::parse_str(&output).expect("Could not converted the rendered output into a \
-    valid token stream");
+    println!("output: {}", output);
+    let token_stream: TokenStream = syn::parse_str(&output)
+        .expect("Could not converted the rendered output into a valid token stream");
     proc_macro::TokenStream::from(token_stream)
 }
 
 
-fn remove_space_added_by_parsing(input: &str) -> String {
-    let graphemes = UnicodeSegmentation::graphemes(input, true);
+fn remove_space_between_tera_brackets_added_by_parsing(input: &str) -> String {
+    let graphemes = input.graphemes(true);
     let mut graphemes_it = graphemes.into_iter();
     let Some(mut prev) = graphemes_it.next() else {
         return String::new();
@@ -68,4 +58,85 @@ fn remove_space_added_by_parsing(input: &str) -> String {
     }
     result.push(this);
     result.join("")
+}
+
+//************************************************************************//
+
+/// parses into the context and rust code strings. Context is either originally json or string json
+fn parse_into_context_and_code(input_as_string: &str, input_as_tokens: proc_macro::TokenStream) -> (String, String) {
+    let first = input_as_string.trim().graphemes(true).next().expect("input is empty");
+    return match first {
+        "{" => parse_json_context(input_as_string),
+        _ => {
+            let (left, right) =
+                parse_string_context(input_as_tokens);
+            return (left.to_owned(), right.to_owned());
+        }
+    };
+}
+
+
+struct TeraMacroStringInput {
+    context: LitStr,
+    rust_code: TokenStream,
+}
+
+impl Parse for TeraMacroStringInput {
+    fn parse(input: ParseStream) -> syn::Result<Self> {
+        let context: LitStr = input.parse().expect("Tried to treat context as a String, but was not a String");
+        input.parse::<syn::token::Comma>().expect("Context and code are not separated by a comma");
+        let rust_code = input.parse()?;
+
+        Ok(TeraMacroStringInput { context, rust_code })
+    }
+}
+
+fn parse_string_context(tokens: proc_macro::TokenStream) -> (String, String) {
+    let parsed: TeraMacroStringInput = syn::parse(tokens).expect("Could not parse context from code.");
+    (parsed.context.value(), parsed.rust_code.to_string())
+}
+
+fn parse_json_context(input: &str) -> (String, String) {
+    let mut brace_count = 0;
+    let mut index = 0;
+    let graphemes = input.graphemes(true).collect::<Vec<&str>>();
+
+    for grapheme in graphemes.as_slice() {
+        if *grapheme == "{" {
+            brace_count += 1;
+        } else if *grapheme == "}" {
+            brace_count -= 1;
+        }
+        index += 1;
+        if brace_count <= 0 {
+            break;
+        }
+    }
+
+    if brace_count != 0 {
+        panic!("Unbalanced braces in context");
+    }
+    if index == 0 {
+        panic!("No context was found");
+    }
+
+    let (left, rust_code_with_comma) = graphemes.split_at(index);
+    let context = left.join("");
+    let mut index = 0;
+    for grapheme in rust_code_with_comma.iter() {
+        if grapheme.chars().all(char::is_whitespace) {
+            continue;
+        }
+        if *grapheme == "," {
+            break;
+        } else {
+            panic!("Context and code are not separated by a comma.")
+        }
+    }
+    index += 1;
+    if index >= rust_code_with_comma.len() {
+        panic!("No Rust code was provided.")
+    }
+
+    (context, rust_code_with_comma[index..].join(""))
 }
--- tests/mod.rs
+++ tests/mod.rs
@@ -1,17 +1,33 @@
 use tera_macro::tera;
 
-tera! {
-    r#"{ "x" : "1" }"#,
-    const w: usize = {{ x }};
-}
-
-
 #[test]
 fn it_works() {
     tera! {
-        r#"{ "x" : "2" }"#,
-        let y: usize = {{ x }};
+        { "x" : "1" },
+        let result: usize = {{ x }};
     }
-    assert_eq!(y, 2);
-    assert_eq!(w, 1);
+    assert_eq!(result, 1);
+
+    tera! {
+        r#"{ "x" : "2" }"#,
+        let result: usize = {{ x }};
+    }
+    assert_eq!(result, 2);
+}
+
+#[test]
+fn for_loop() {
+    tera! {
+        { "x" : 20 },
+        let result: Vec<usize> = vec![
+        {% for number in range(end=x) %}
+            {% if number % 2 == 0 %}
+                {{ number }},
+            {% endif %}
+        {% endfor %}
+        ];
+    }
+
+    let expected: Vec<usize> = vec![0, 2, 4, 6, 8, 10, 12, 14, 16, 18];
+    assert_eq!(result, expected);
 }
